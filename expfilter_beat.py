7635ee503eef93d59d74dacfd06f0798624994ff
HASEBE Masahiko <JCA03205@gmail.com>
2022-09-20 20:50:11 +0900

    拍フィルタの微調整、パラメータ追加

--- expfilter_beat.py
+++ expfilter_beat.py
@@ -3,34 +3,41 @@ import expfilter as ef
 
 class BeatFilter(ef.ExpfilterIF):
 
+    EFFECT = 10     # bigger(1..100), stronger
+    MIN_VEL = 70
+
     def __init__(self):
         pass
 
     def filtering(self, inputs, bpm, tick_for_onemsr):
-        if bpm < 70:
+        if bpm < self.MIN_VEL:
             return inputs
 
-        # とりあえず、純粋な四拍子、三拍子は救うことにする
+        # 純粋な四拍子、三拍子のみ救う
         outputs = []
-        base_bpm = (bpm-70)//10
+        base_bpm = (bpm-self.MIN_VEL)*self.EFFECT/100
         if tick_for_onemsr == 1920:
             for dt in inputs:
                 tm = (dt[0] % 1920)/480
                 if tm == 0.0:
-                    dt[2] += base_bpm
-                elif tm == 1.0 or tm == 3.0:
-                    dt[2] += base_bpm//4
+                    dt[2] += int(base_bpm)
+                #elif tm == 1.0 or tm == 3.0:
+                #    pass
                 elif tm == 2.0:
-                    dt[2] += base_bpm//2
+                    dt[2] += int(base_bpm/4)
+                else:
+                    dt[2] -= int(base_bpm/4)
                 outputs.append(dt)
         elif tick_for_onemsr == 1440:
             for dt in inputs:
                 tm = (dt[0] % 1440)/480
                 if tm == 0.0:
-                    dt[2] += base_bpm
+                    dt[2] += int(base_bpm)
                 elif tm == 1.0:
-                    dt[2] += base_bpm//2
-                elif tm == 2.0:
-                    dt[2] += base_bpm//4
+                    dt[2] += int(base_bpm/4)
+                #elif tm == 2.0:
+                #    pass
+                else:
+                    dt[2] -= int(base_bpm/4)
                 outputs.append(dt)
         return outputs
